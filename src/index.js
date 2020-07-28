373b6b35ee13ce0799d360012abf4956d5fac29d
Samuel Walker <sewalker191@gmail.com>
2020-07-28 10:26:06 -0700

    Add MovingObject constructor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const MovingObject = require('./moving_object');
+
 console.log('webpack is running');
+
+const object1 = new MovingObject({ 
+    pos: [300, 300], 
+    vel: [10,10],
+    radius: 5,
+    color: "#00FF00"
+});
+
+console.log(object1);
---
+++ src/moving_object.js
@@ -0,0 +1,10 @@
+class MovingObject {
+    constructor(options) {
+        this.pos = options.pos;
+        this.vel = options.vel;
+        this.radius = options.radius;
+        this.color = options.color;
+    }
+}
+
+module.exports = MovingObject;
